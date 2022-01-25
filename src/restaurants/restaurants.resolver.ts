ce618ffade4889a00631b9f3fee69dd0963aed6a
dja1716 <hdum@uwaterloo.ca>
2022-01-25 13:54:59 +0900

    InputTypes & ArgType

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,16 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
   @Query((returns) => [Restaurant])
   restuarants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
   }
 }
