9e9e9f8655b0b3e8cafbed285245a46132b76e70
dja1716 <hdum@uwaterloo.ca>
2022-01-26 13:07:28 +0900

    injecting repository: restaurant

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,16 +1,17 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
+  constructor(private readonly RestaurantService: RestaurantService) {}
   @Query((returns) => [Restaurant])
-  restuarants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restuarants(): Promise<Restaurant[]> {
+    return this.RestaurantService.getAll();
   }
   @Mutation((returns) => Boolean)
   createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
-    console.log(createRestaurantDto);
     return true;
   }
 }
